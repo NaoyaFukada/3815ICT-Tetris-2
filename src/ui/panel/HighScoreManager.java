2993dc222be6ab1e0b4722a8f95f953483f2458c
Ryotankobu <ucp4fya7@yahoo.co.jp>
2024-09-25 14:49:49 +1000

    commit coz I can't change branches without it

--- src/ui/panel/HighScoreManager.java
+++ src/ui/panel/HighScoreManager.java
@@ -1,2 +1,66 @@
-package ui.panel;public class HighScoreManager {
+package ui.panel;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class HighScoreManager {
+    private static HighScoreManager instance;
+    private List<HighScoreEntry> highScores;
+
+    // Private constructor to prevent instantiation
+    private HighScoreManager() {
+        loadHighScores();  // Load the scores once when the instance is created
+    }
+
+    // Static method to get the single instance of HighScoreManager
+    public static HighScoreManager getInstance() {
+        if (instance == null) {
+            instance = new HighScoreManager();
+        }
+        return instance;
+    }
+
+    // Method to load high scores from the JSON file
+    private void loadHighScores() {
+        Gson gson = new Gson();
+        try {
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("data/highscores.json");
+
+            if (inputStream == null) {
+                System.out.println("Could not find the highscores.json file.");
+                return;
+            }
+
+            InputStreamReader reader = new InputStreamReader(inputStream);
+            Type highScoreListType = new TypeToken<List<HighScoreEntry>>() {}.getType();
+            highScores = gson.fromJson(reader, highScoreListType);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Public method to get the list of high scores
+    public List<HighScoreEntry> getHighScores() {
+        return highScores;
+    }
+
+    // Inner class to represent a high score entry
+    public static class HighScoreEntry {
+        private String playerName;
+        private int score;
+
+        public String getPlayerName() {
+            return playerName;
+        }
+
+        public int getScore() {
+            return score;
+        }
+    }
 }
--- src/ui/panel/HighScorePanel.java
+++ src/ui/panel/HighScorePanel.java
@@ -1,16 +1,10 @@
 package ui.panel;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import ui.MainFrame;
 import util.UIUtils;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.lang.reflect.Type;
-import java.util.List;
 
 public class HighScorePanel extends JPanel {
 
@@ -21,12 +15,10 @@ public class HighScorePanel extends JPanel {
         titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT); // Center the title
         add(titleLabel);
 
-        // Load high scores from JSON file
-        List<HighScoreEntry> highScores = loadHighScores();
-
-        // Display high scores
-        if (highScores != null) {
-            for (HighScoreEntry entry : highScores) {
+        // Get the high scores from the singleton
+        HighScoreManager highScoreManager = HighScoreManager.getInstance();
+        if (highScoreManager.getHighScores() != null) {
+            for (HighScoreManager.HighScoreEntry entry : highScoreManager.getHighScores()) {
                 add(new JLabel(entry.getPlayerName() + " - " + entry.getScore()));
             }
         } else {
@@ -43,53 +35,5 @@ public class HighScorePanel extends JPanel {
         backButton.addActionListener(e -> MainFrame.MAIN_FRAME.showScreen("Main"));
         add(backButton);
         add(Box.createVerticalStrut(20)); // Add some space below the button
-    }
-
-    // Method to load high scores from JSON
-    private List<HighScoreEntry> loadHighScores() {
-        Gson gson = new Gson();
-        try {
-            // Load the file from the classpath
-            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("data/highscores.json");
-
-            if (inputStream == null) {
-                System.out.println("Could not find the highscores.json file.");
-                return null;
-            }
-
-            InputStreamReader reader = new InputStreamReader(inputStream);
-            Type highScoreListType = new TypeToken<List<HighScoreEntry>>() {}.getType();
-            List<HighScoreEntry> highScores = gson.fromJson(reader, highScoreListType);
-
-            // Debugging: Print the list of high scores
-            if (highScores != null) {
-                System.out.println("High scores loaded: ");
-                for (HighScoreEntry entry : highScores) {
-                    System.out.println(entry.getPlayerName() + " - " + entry.getScore());
-                }
-            } else {
-                System.out.println("No high scores found in the file.");
-            }
-
-            return highScores;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    // Inner class to represent a high score entry
-    static class HighScoreEntry {
-        private String playerName;
-        private int score;
-
-        public String getPlayerName() {
-            return playerName;
-        }
-
-        public int getScore() {
-            return score;
-        }
     }
 }
